4b9dfe04eba2e189eb52f20a9ac84fd21fc4ff4a
benaturalblue <yuuka.nakazaki513@gmail.com>
2023-06-02 19:01:06 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#show"
-  resources :orders, only: [:new, :create]
+  resources :orders, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :order_items, only: [:new, :create]
 end
