2999d6d2f634a60dae317b9be036d7ca978ccba7
benaturalblue <yuuka.nakazaki513@gmail.com>
2023-06-04 17:16:34 +0900

    ログアウト機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: "users#show"
   resources :orders, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :order_items, only: [:new, :create]
